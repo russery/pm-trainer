a045553af8910755b7e93a5c5ff9a46be6e5c248
Robert Ussery <russery@gmail.com>
2020-11-30 08:22:14 -0800

    made plotter a class, changed to use native pysimplegui plotting instead of mpl

--- profile_plotter.py
+++ profile_plotter.py
@@ -1,70 +1,62 @@
-import matplotlib
-matplotlib.use('TkAgg')
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
 import PySimpleGUI as sg
 import numpy as np
 from workout_profile import Workout
 
-
-ZONES = [0, 0.75, 0.9,1.05,1.2,100]
-ZONE_COLORS = ["blue", "green", "yellow", "orange", "red"]
-
 def _get_zone(power):
-	for i in range (0,len(ZONES)-1):
-		print(i)
-		if (power > ZONES[i]) and (power <= ZONES[i+1]):
-			return i
-	return None
+    for i in range (0,len(ProfilePlot.ZONES)-1):
+        if (power > ProfilePlot.ZONES[i]) and (power <= ProfilePlot.ZONES[i+1]):
+            return i
+    return None
+
+class ProfilePlot():
+    ZONES = [0, 0.75, 0.9,1.05,1.2,100]
+    ZONE_COLORS = ["blue", "green", "yellow", "orange", "red"]
+
+    def __init__(self, graph):
+        self.graph = graph
+
+    def plot_blocks(self,all_blocks):
+        dur = 0
+        gwidth, gheight = self.graph.Size
+        gheight = gheight * 0.5 # TODO: get max power in all blocks and use this to set Y scale factor
+        for block in all_blocks:
+            print (block)
+            width = block[0]
+            start = block[1]
+            end = block[2]
+            zone = _get_zone(np.average([start,end]))
+            color = ProfilePlot.ZONE_COLORS[zone]
+            startx = dur*gwidth
+            starty = start*gheight
+            endx = (dur+width) * gwidth
+            endy = end*gheight
+            print(startx, starty, endx, endy, gwidth, gheight)
+            poly = self.graph.draw_polygon((
+                (startx,0), (startx,starty), (endx,endy), (endx,0)),
+                fill_color=color)
+            dur += width
 
 
 if __name__ == '__main__':
-	workout = Workout("workouts/short_stack.yaml")
-	all_blocks = workout.get_all_blocks()
-	plt.axes()
+    workout = Workout("workouts/short_stack.yaml")
+    all_blocks = workout.get_all_blocks()
 
+    layout = [[sg.Graph(
+        canvas_size=(800, 200),
+        graph_bottom_left=(0, 0),
+        graph_top_right=(800, 200),
+        background_color='gray',
+        enable_events=True,
+        key='-PROFILE-')]]
+    window = sg.Window('Workout Profile', layout, finalize=True)
 
-	dur = 0
-	for block in all_blocks:
-		print (block)
-		width = block[0]
-		start = block[1]
-		end = block[2]
-		zone = _get_zone(np.average([start,end]))
-		color = ZONE_COLORS[zone]
-		if start == end:
-			# Start == end, so this is a rectangle
-			shape = plt.Rectangle((dur, 0), width, start, fc=color)
-		else:
-			# This is a ramp (triangle)
-			shape = plt.Polygon([[dur,0],[dur,start],[dur+width,end],[dur+width,0]],fc=color)
-		plt.gca().add_patch(shape)
-		dur += width
+    plot = ProfilePlot(graph=window['-PROFILE-'])
+    plot.plot_blocks(all_blocks)
+        
 
-	plt.axis('scaled')
-
-
-	fig = plt.gcf()
-	figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds
-
-
-	def draw_figure(canvas, figure, loc=(0, 0)):
-	    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-	    figure_canvas_agg.draw()
-	    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-
-
-	# define the window layout
-	layout = [[sg.Text('Plot test', font='Any 18')],
-	          [sg.Canvas(size=(figure_w, figure_h), key='canvas')],
-	          [sg.OK(pad=((figure_w / 2, 0), 3), size=(4, 2))]]
-
-	# create the form and show it without the plot
-	window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI', layout, force_toplevel=True, finalize=True)
-
-	# add the plot to the window
-	fig_photo = draw_figure(window['canvas'].TKCanvas, fig)
-
-	event, values = window.read()
-
-
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event == sg.WIN_CLOSED:
+            break
+    window.close()
