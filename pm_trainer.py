2f620ec247a1de419da44692708913a32a72b4c5
russery <russery@gmail.com>
2021-03-11 09:25:32 -1000

    Renamed, general cleanup

--- pm_trainer.py
+++ pm_trainer.py
@@ -1,5 +1,7 @@
 '''
-Logs trainer rides or something.
+Power-Mode Trainer:
+Logs bike trainer rides, enables displaying a workout profile with
+target power segments, and interfaces to bike trainer sensors over ANT+.
 '''
 import argparse
 import os
@@ -25,7 +27,7 @@ DEFAULT_SETTINGS = {
 
    # Window / system settings
    "LogDirectory": "./logs",
-   "SettingsFile": "zwerft_settings.ini",
+   "SettingsFile": "pm_trainer_settings.ini",
    "UpdateRateHz": 10
 }
 
@@ -50,7 +52,7 @@ else:
 
 class Timer():
     '''
-    A timer, that returns time as a datetime timedelta. The timer only updates
+    A timer that returns time as a datetime timedelta. The timer only updates
     when the update() method is called, so that the same time can be used in
     multiple places in a loop.
     '''
@@ -103,7 +105,7 @@ def _avg_val(running_avg_val, new_val, avg_window=10):
     diff = new_val - running_avg_val
     return running_avg_val + (diff / avg_window)
 
-def _exit_zwerft(status=0):
+def _exit_app(status=0):
     '''
     Exit cleanly, closing window, writing logfile, and freeing ANT+ resources.
     '''
@@ -254,10 +256,10 @@ if not REPLAY_MODE:
                  "- check USB connection and try again",
                     custom_text="Exit", line_width=50, keep_on_top=True, any_key_closes=True)
                 # TODO: Keep the application open and try again - this should be recoverable
-                _exit_zwerft(-1)
+                _exit_app(-1)
             else:
                 print("Caught sensor error {}".format(e.err_type))
-                _exit_zwerft(-1)
+                _exit_app(-1)
         time.sleep(1)
         sensors.close()
         del sensors
@@ -296,7 +298,7 @@ layout = [[sg.T("HH:MM:SS", (8,1), pad=((20,20),(5,0)),
            sg.Graph(canvas_size=(1000,60), graph_bottom_left=(0,0),
                      graph_top_right=(1000,60), background_color="black",
                      key="-PROFILE-")]]
-window = sg.Window("Zwerft", layout, keep_on_top=True, use_ttk_buttons=True,
+window = sg.Window("PM Trainer", layout, keep_on_top=True, use_ttk_buttons=True,
     alpha_channel=0.9, finalize=True, element_padding=(0,0))
 power_bug = BugIndicator(window["-BUG-"])
 power_bug.add_bug("TARGET_POWER", level_percent=0.5, color="blue")
@@ -336,7 +338,7 @@ while True:
         # Handle window events
         event, _ = window.read(timeout=update_ms)
         if event == sg.WIN_CLOSED:
-            _exit_zwerft()
+            _exit_app()
         if event == "-SETTINGS-":
             #window.Disappear()
             _settings_dialog(cfg)
@@ -424,7 +426,6 @@ while True:
             _plot_trace(window["-PROFILE-"],
                 (norm_time, avg_power / ftp_watts),
                 (min_power, max_power), color="red")
-
 
         # Update power bug
         if power:
