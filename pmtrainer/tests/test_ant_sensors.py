a2c39f0a3cfba5cebd366761ce90d5ec6f10a7d8
russery <russery@gmail.com>
2021-03-25 15:44:37 -0700

    abandoned sensor mock for now

--- pmtrainer/tests/test_ant_sensors.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from unittest.mock import patch
-from ..ant_sensors import AntSensors
-
-class TestAntSensors(unittest.TestCase):
-    @patch("ant.plus.power.BicyclePower")
-    def test_initialization(self, antmock):
-        sensors = AntSensors()
-
